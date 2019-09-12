b18506ce4faab9dcc07c2dffdae3ed2d6808bc92
Cheng Shi <t-chsh@microsoft.com>
2019-09-12 18:08:46 +0800

    update 66, 69, 70

---
+++ 66.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+public:
+    vector<int> plusOne(vector<int> &digits)
+    {
+        int n = digits.size();
+        digits[n - 1] += 1;
+        for (int i = n - 1; i >= 0; --i)
+        {
+            if (digits[i] == 10 && i - 1 >= 0)
+            {
+                digits[i] = 0;
+                digits[i - 1] += 1;
+            }
+        }
+        if (digits[0] == 10)
+        {
+            digits[0] = 0;
+            digits.insert(digits.begin(), 1);
+        }
+        return digits;
+    }
+};
---
+++ 70.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+public:
+    int climbStairs(int n)
+    {
+        if (n == 1)
+            return 1;
+        int a = 1, b = 1, c = 2;
+        for (int i = 0; i < n - 1; ++i)
+        {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return c;
+    }
+};
